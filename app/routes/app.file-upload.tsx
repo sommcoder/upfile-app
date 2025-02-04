7182aef8294f568237c54b399fcc169cfb802ef4
Brian Davies <brian.davies589@gmail.com>
2025-02-04 13:50:28 -0500

    nothing much, just saving before work.

--- app/routes/app.file-upload.tsx
+++ app/routes/app.file-upload.tsx
@@ -7,14 +7,14 @@ import {
 import { randomUUID } from "node:crypto";
 import { authenticate, db } from "app/shopify.server";
 import { LocalFileStorage } from "@mjackson/file-storage/local";
-import { Readable, Writable } from "node:stream";
+import { Readable, Stream, Writable } from "node:stream";
 import fs from "node:fs";
 import Busboy from "busboy";
 import path from "node:path";
 
-const fileStorage = new LocalFileStorage(
-  await fsp.mkdtemp(path.join(os.tmpdir(), "form-data-parser-")),
-);
+// const fileStorage = new LocalFileStorage(
+//   await fsp.mkdtemp(path.join(os.tmpdir(), "form-data-parser-")),
+// );
 
 export async function action({ request }: ActionFunctionArgs) {
   try {
@@ -26,6 +26,25 @@ export async function action({ request }: ActionFunctionArgs) {
     const storeURL = session.shop;
     if (!storeId || !storeURL)
       return new Response("Invalid session", { status: 400 });
+
+    // request.body is a ReadableStream
+
+    const fileId = randomUUID();
+    const chunks: Buffer[] = [];
+
+    const writeableStream = new WritableStream({
+      write(chunk) {
+        console.log("chunk:", chunk);
+      },
+    });
+
+    // const writeStream = fs.createWriteStream(`uploads/${fileId}`);
+    // request.body?.pipeTo();
+    request.body?.pipeTo(writeableStream);
+
+    // request.on("data", (chunk) => {
+    //   chunks.push(chunk);
+    // });
 
     const contentType = request.headers.get("content-type") || "";
     if (!contentType.includes("multipart/form-data")) {
